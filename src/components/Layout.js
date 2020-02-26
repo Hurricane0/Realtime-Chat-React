d71cd65277321c8e075abb0bf0da6176bab17ce5
Hurricane0 <nikitadv777@gmail.com>
2020-02-26 22:36:57 +0200

    url fixes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,71 +1,68 @@
-import React, { Component } from 'react';
-import io from 'socket.io-client'
-import { USER_CONNECTED, LOGOUT } from '../Events'
-import LoginForm from './LoginForm'
-import ChatContainer from './chats/ChatContainer'
+import React, { Component } from "react";
+import io from "socket.io-client";
+import { USER_CONNECTED, LOGOUT } from "../Events";
+import LoginForm from "./LoginForm";
+import ChatContainer from "./chats/ChatContainer";
 
-const socketUrl = "http://localhost:3231"
+const socketUrl = "/";
 export default class Layout extends Component {
-	
-	constructor(props) {
-	  super(props);
-	
-	  this.state = {
-	  	socket:null,
-	  	user:null
-	  };
-	}
+  constructor(props) {
+    super(props);
 
-	componentWillMount() {
-		this.initSocket()
-	}
+    this.state = {
+      socket: null,
+      user: null
+    };
+  }
 
-	/*
-	*	Connect to and initializes the socket.
-	*/
-	initSocket = ()=>{
-		const socket = io(socketUrl)
+  componentWillMount() {
+    this.initSocket();
+  }
 
-		socket.on('connect', ()=>{
-			console.log("Connected");
-		})
-		
-		this.setState({socket})
-	}
+  /*
+   *	Connect to and initializes the socket.
+   */
+  initSocket = () => {
+    const socket = io(socketUrl);
 
-	/*
-	* 	Sets the user property in state 
-	*	@param user {id:number, name:string}
-	*/	
-	setUser = (user)=>{
-		const { socket } = this.state
-		socket.emit(USER_CONNECTED, user);
-		this.setState({user})
-	}
+    socket.on("connect", () => {
+      console.log("Connected");
+    });
 
-	/*
-	*	Sets the user property in state to null.
-	*/
-	logout = ()=>{
-		const { socket } = this.state
-		socket.emit(LOGOUT)
-		this.setState({user:null})
+    this.setState({ socket });
+  };
 
-	}
+  /*
+   * 	Sets the user property in state
+   *	@param user {id:number, name:string}
+   */
 
+  setUser = user => {
+    const { socket } = this.state;
+    socket.emit(USER_CONNECTED, user);
+    this.setState({ user });
+  };
 
-	render() {
-		const { title } = this.props
-		const { socket, user } = this.state
-		return (
-			<div className="container">
-				{
-					!user ?	
-					<LoginForm socket={socket} setUser={this.setUser} />
-					:
-					<ChatContainer socket={socket} user={user} logout={this.logout}/>
-				}
-			</div>
-		);
-	}
+  /*
+   *	Sets the user property in state to null.
+   */
+  logout = () => {
+    const { socket } = this.state;
+    socket.emit(LOGOUT);
+    this.setState({ user: null });
+  };
+
+  render() {
+    const { title } = this.props;
+    const { socket, user } = this.state;
+    return (
+      <div className="container">
+        {!user ? (
+          <LoginForm socket={socket} setUser={this.setUser} />
+        ) : (
+          <ChatContainer socket={socket} user={user} logout={this.logout} />
+        )}
+      </div>
+    );
+  }
 }
